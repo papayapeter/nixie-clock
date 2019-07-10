34c9b18a81a63ff3a0840232c247d7665ebaa046
papayapeter <t.babel@gmx.com>
2019-07-10 11:45:15 +0200

    sourced clock Class out

--- atmega328p/atmega328p.ino
+++ atmega328p/atmega328p.ino
@@ -1,4 +1,4 @@
-minute_on// nixie clock r1.1
+// nixie clock r1.1
 //
 // zeno gries
 
@@ -6,13 +6,24 @@ minute_on// nixie clock r1.1
 #define DEBUG
 
 // libraries -------------------------------------------------------------------
-#include <MCP7940.h>
 #include <Metro.h>
-#include <Bounce2.h>
-#include <Shifty.h>
+#include "clock.h"
 
 // settings -------------------------------------------------------------------
 uint64_t debounce_delay = 100;
+
+// bit table for tube driver
+const uint8_t table[11][4] = {{0, 0, 0, 0},  // 0
+                              {1, 0, 0, 1},  // 1
+                              {0, 0, 0, 1},  // 2
+                              {1, 1, 1, 0},  // 3
+                              {0, 1, 1, 0},  // 4
+                              {1, 0, 1, 0},  // 5
+                              {0, 0, 1, 0},  // 6
+                              {1, 1, 0, 0},  // 7
+                              {0, 1, 0, 0},  // 8
+                              {1, 0, 0, 0},  // 9
+                              {1, 1, 1, 1}}; // NONE
 
 // pins ------------------------------------------------------------------------
 const uint8_t DATA  = 2;
@@ -25,276 +36,169 @@ const uint8_t SWITCH_1 = 6; // right
 const uint8_t SWITCH_2 = 7; // left up
 const uint8_t SWITCH_3 = 8; // left down
 
-// classes ---------------------------------------------------------------------
-class Clock
-{
-    // pins
-    uint8_t pin_dot;
-
-    // objects
-    Shifty shift;
-    MCP7940_Class real_time;
-
-    DateTime now;
-    DateTime last;
-
-    Bounce button_1;
-    Bounce button_2;
-    Bounce button_3;
-  public:
-    /**
-     names for button states
-    */
-    const uint8_t pressed_1;
-    const uint8_t pressed_2;
-    const uint8_t pressed_3;
-
-    /**
-     @brief   creates and instance of clock
-
-     sets all the pins and debounced switches and connects with rtc
-
-     @ param  debounce  sets debounce time for buttons in milliseconds
-    */
-    Clock(uint8_t data, uint8_t clock, uint8_t latch, uint8_t dot,
-          uint8_t switch_1, uint8_t switch_2, uint8_t switch_3,
-          uint16_t debounce);
-    /**
-     @brief   fetches time from the rtc
-    */
-    bool updateTime();
-    /**
-     @brief   sets time on the rtc
-    */
-    void setTime(uint8_t hour, uint8_t minute)
-    /**
-     @brief   checks for button presses
-
-     @return  returns name for button states (or 0 if nothing was pressed)
-    */
-    uint8_t updateButtons();
-    /**
-     @brief   sets dot on or off
-    */
-    void setDot(bool state);
-    /**
-     @brief   displays time on nixie tubes
-
-     uses time from the now object in the class
-    */
-    bool display();
-    /**
-     @brief   displays time on nixie tubes
-
-     @params  hour_on, minute_on  turns tubes for hours or minutes on or off
-
-     @return  returns false if hour or minute out of bounds
-    */
-    bool display(uint8_t hour, uint8_t minute,
-                 bool hour_on = true, bool minute_on = true);
-}
-
 // objects ---------------------------------------------------------------------
-Metro debug_time(100);
+Metro time(10);
+Metro dot(10);
+Metro buttons(5);
 
-Clock clock(DATA, CLOCK, LATCH, DOT, SWITCH_1, SWITCH_2, SWITCH_3, debounce_delay);
+Clock clock(DATA, CLOCK, LATCH, DOT,
+            SWITCH_1, SWITCH_2, SWITCH_3,
+            debounce_delay, table);
 
 // variables -------------------------------------------------------------------
+const uint8_t STATE_NORMAL      = 0;
+const uint8_t STATE_CLOCK_OFF   = 1;
+const uint8_t STATE_SET_HOUR    = 2;
+const uint8_t STATE_SET_MINUTE  = 3;
+
+uint8_t state = STATE_NORMAL;
+
+bool first_on     = true;
+bool first_off    = false;
+bool first_hour   = false;
+bool first_minute = false;
+
+uint8_t set_hour;
+uint8_t set_minute;
 
 // setup -----------------------------------------------------------------------
 void setup()
 {
-
+  DEBUG_INIT(9600);
 }
 
 // loop ------------------------------------------------------------------------
 void loop()
 {
-
-}
-
-// methods ---------------------------------------------------------------------
-Clock::Clock(uint8_t data, uint8_t clock, uint8_t latch, uint8_t dot,
-      uint8_t switch_1, uint8_t switch_2, uint8_t switch_3, uint16_t debounce)
-      : pin_dot(dot), pressed_1(1), pressed_2(2), pressed_3(3)
-{
-  // debug
-  #ifdef DEBUG
-    Serial.begin(9600);
-  #endif
-
-  // set pin mode
-  pinMode(pin_dot, OUTPUT);
-
-  // set debounced buttons
-  button_1.attach(switch_1, INPUT_PULLUP);
-  button_2.attach(switch_2, INPUT_PULLUP);
-  button_3.attach(switch_3, INPUT_PULLUP);
-  button_1.interval(debounce);
-  button_2.interval(debounce);
-  button_3.interval(debounce);
-
-  // initialize shift registers
-  shift.setBitCount(16);
-  shift.setPins(data, clock, latch);
-
-  // set diplay to zero with dot on
-  this->display(0, 0);
-  digitalWrite(pin_dot, HIGH);
-
-  // begin communication with rtc and start oscillator
-  while (!real_time.begin())
+  // normal clock reading and updating
+  if (state == STATE_NORMAL)
   {
-    // if cannot connect to rtc -> blink fast
-    digitalWrite(pin_dot, HIGH);
-    delay(500);
-    digitalWrite(pin_dot, LOW);
-    delay(500);
-
-    // debug
-    #ifdef DEBUG
-      Serial.println("cannot connect. reattempting...");
-    #endif
+    if (first_on) // if on for the first time -> update and display
+    {
+      clock.updateTime();
+      clock.display();
+      first_on = false;
+    }
+    // check time and update tubes
+    if (time.check())
+    {
+      if (clock.updateTime())
+        clock.display();
+    }
+    // blink dot every second
+    if (dot.check())
+    {
+      clock.setDot(clock.evenSecond());
+    }
+    // check buttons
+    if (buttons.check())
+    {
+      uint8_t button_state = clock.updateButtons();
+      if (button_state == clock.pressed_2)
+      {
+        state = STATE_CLOCK_OFF;
+        first_off = true;
+      }
+      else if (button_state == clock.pressed_1)
+      {
+        state = STATE_SET_HOUR;
+        first_hour = true;
+      }
+    }
   }
-  // if connected -> turn dot on and start rtc
-  real_time.deviceStart();
-  digitalWrite(pin_dot, HIGH);
-
-  // debug
-  #ifdef DEBUG
-    Serial.println("connected");
-  #endif
-
-  // update and display the time
-  this->update();
-  this->display();
-}
-
-bool Clock::updateTime()
-{
-  // get time from rtc
-  now = real_time.now();
-
-  // debug
-  #ifdef DEBUG
-    Serial.println("H: " + String(now.hour()) +
-                 "\tM: " + String(now.minute()) +
-                 "\tS: " + String(now.second()));
-  #endif
-
-  // return true if time has changed
-  if (now.minute() != last.minute() || now.hour() != last.hour()) // if time changed
+  // clock off
+  else if (state == STATE_CLOCK_OFF)
   {
-    last = now;
-    return true;
+    if (first_off) // if off for the first time -> turn off
+    {
+      clock.display(0, 0, false, false);
+      clock.setDot(false);
+      first_off = false;
+    }
+    // check buttons
+    if (buttons.check())
+    {
+      uint8_t button_state = clock.updateButtons();
+      if (button_state == clock.pressed_2)
+      {
+        state = STATE_NORMAL;
+        first_on = true;
+      }
+    }
   }
-
-  return false;
-}
-
-void Clock::setTime(uint8_t hour, uint8_t minute)
-{
-  real_time.adjust(DateTime(2019, 3, 23, hour, minute, 0));
-}
-
-uint8_t Clock::updateButtons()
-{
-  button_1.update();
-  button_2.update();
-  button_3.update();
-
-  if (button_1.rose() || button_1.fell()) return pressed_1;
-  if (button_2.rose() || button_2.fell()) return pressed_2;
-  if (button_3.rose() || button_3.fell()) return pressed_3;
-  return 0;
-}
-
-void Clock::setDot(bool state)
-{
-  if (state) digitalWrite(pin_dot, HIGH);
-  else       digitalWrite(pin_dot, LOW);
-}
-
-bool Clock::display()
-{
-  return this->display(now.hour(), now.minute(), true, true);
-}
-
-bool Clock::display(uint8_t hour, uint8_t minute,
-                    bool hour_on = true, bool minute_on = true)
-{
-  // bit table for tube driver
-  const uint8_t table[11][4] = {{0, 0, 0, 0},  // 0
-                                {1, 0, 0, 1},  // 1
-                                {0, 0, 0, 1},  // 2
-                                {1, 1, 1, 0},  // 3
-                                {0, 1, 1, 0},  // 4
-                                {1, 0, 1, 0},  // 5
-                                {0, 0, 1, 0},  // 6
-                                {1, 1, 0, 0},  // 7
-                                {0, 1, 0, 0},  // 8
-                                {1, 0, 0, 0},  // 9
-                                {1, 1, 1, 1}}; // NONE
-
-  // test out of bounds
-  if (hour > 99 || minute > 99)
+  // set hour
+  else if (state == STATE_SET_HOUR)
   {
-    return false;
+    if (first_hour) // if on set hour for the first time
+    {
+      set_hour = clock.getHour();
+      clock.display(set_hour, 0, true, false);
+      first_hour = false;
+    }
+    // blink dot every second
+    if (dot.check())
+    {
+      clock.setDot(clock.evenSecond());
+    }
+    // check buttons
+    if (buttons.check())
+    {
+      uint8_t button_state = clock.updateButtons();
+      // up
+      if (button_state == clock.pressed_2)
+      {
+        if (++set_hour > 24) set_hour = 0;
+        clock.display(set_hour, 0, true, false);
+      }
+      // down
+      if (button_state == clock.pressed_3)
+      {
+        if (--set_hour < 0) set_hour = 24;
+        clock.display(set_hour, 0, true, false);
+      }
+      else if (button_state == clock.pressed_1)
+      {
+        state = STATE_SET_MINUTE;
+        first_minute = true;
+      }
+    }
   }
-
-  uint8_t h_1;
-  uint8_t h_2;
-  uint8_t m_1;
-  uint8_t m_2;
-  // get single digits or turn display off
-  if (hour_on)
+  // set minute
+  else if (state == STATE_SET_MINUTE)
   {
-    h_1 = hour / 10;
-    h_2 = hour % 10;
+    if (first_minute) // if on set minute for the first time
+    {
+      set_minute = clock.getMinute();
+      clock.display(0, set_minute, false, true);
+      first_minute = false;
+    }
+    // blink dot every second
+    if (dot.check())
+    {
+      clock.setDot(clock.evenSecond());
+    }
+    // check buttons
+    if (buttons.check())
+    {
+      uint8_t button_state = clock.updateButtons();
+      // up
+      if (button_state == clock.pressed_2)
+      {
+        if (++set_minute > 60) set_minute = 0;
+        clock.display(0, set_minute, false, true);
+      }
+      // down
+      if (button_state == clock.pressed_3)
+      {
+        if (--set_minute < 0) set_minute = 24;
+        clock.display(0, set_minute, false, true);
+      }
+      else if (button_state == clock.pressed_1)
+      {
+        clock.setTime(set_hour, set_minute);
+        state = STATE_NORMAL;
+        first_on = true;
+      }
+    }
   }
-  else
-  {
-    h_1 = 10;
-    h_2 = 10;
-  }
-  if (minute_on)
-  {
-    m_1 = minute / 10;
-    m_2 = minute % 10;
-  }
-  else
-  {
-    m_1 = 10;
-    m_2 = 10;
-  }
-
-  shift.batchWriteBegin();
-
-  // m1
-  shift.writeBit(0, table[m_1][0]);
-  shift.writeBit(1, table[m_1][1]);
-  shift.writeBit(2, table[m_1][2]);
-  shift.writeBit(3, table[m_1][3]);
-
-  // m2
-  shift.writeBit(4, table[m_2][0]);
-  shift.writeBit(5, table[m_2][1]);
-  shift.writeBit(6, table[m_2][2]);
-  shift.writeBit(7, table[m_2][3]);
-
-  // h1
-  shift.writeBit(8, table[h_1][0]);
-  shift.writeBit(9, table[h_1][1]);
-  shift.writeBit(10, table[h_1][2]);
-  shift.writeBit(11, table[h_1][3]);
-
-  // h2
-  shift.writeBit(12, table[h_2][0]);
-  shift.writeBit(13, table[h_2][1]);
-  shift.writeBit(14, table[h_2][2]);
-  shift.writeBit(15, table[h_2][3]);
-
-  shift.batchWriteEnd();
-
-  return true;
 }
